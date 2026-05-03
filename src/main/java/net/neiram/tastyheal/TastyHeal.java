2d3d9d88ea0da5e1d65dae7bce9c37abd3810dd2
madebyNeiram <neiram.ytb@gmail.com>
2026-05-04 01:05:00 +0200

    Initial commit

---
+++ src/main/java/net/neiram/tastyheal/TastyHeal.java
@@ -0,0 +1,17 @@
+package net.neiram.tastyheal;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TastyHeal implements ModInitializer {
+	public static final String MOD_ID = "tastyheal";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/neiram/tastyheal/TastyHealClient.java
@@ -0,0 +1,10 @@
+package net.neiram.tastyheal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TastyHealClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
